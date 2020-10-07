d2c66ec41e816726623cf020f5df1b947d93f00f
ddp1234 <72114236+ddp1234@users.noreply.github.com>
2020-10-07 10:26:10 +0800

    Update thread.java

--- src/org/lins/Thread/thread.java
+++ src/org/lins/Thread/thread.java
@@ -5,9 +5,8 @@ public class thread {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-System.out.println("ÄãºÃ°¡£¬´ó¼ÒºÃ");
 	System.out.println("123");
-		System.out.println("456");
+	System.out.println("456");
 	}
 
 }
